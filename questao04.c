66fe6c1a9c894132cf062380937af4a1a9623b71
Maurício Rômulo <105665865+Mauricio41@users.noreply.github.com>
2022-07-11 21:43:54 -0300

    Update questao04.c

--- questao04.c
+++ questao04.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int z,x,a,b,m,n,w,y,a1,b1;
-    int m1[4][5],soma;
+    int z,x,a,b
+    int m1[3][5],soma[3];
    
     printf("\tSoma do Vetor:\n");
     printf("\n\nDigite o vetor A:\n");
-    for(z=0;z<4;z++){
+    for(z=0;z<3;z++){
         for(x=0;x<5;x++){
             printf("posição [%d][%d]:  ",z,x);
             scanf("%d",&m1[z][x]);
         }
     }
     
-    for(a=0;a<4;a++){
+    for(a=0;a<3;a++){
         for(b=0;b<5;b++){
-            soma=soma+m1[a][b];
+                soma[a]=soma[a]+m1[a][b];
         }
     }
-    printf("\n\nSoma do vetor é %d: ",soma);
+    printf("\nA soma linear das matrizes e:\n");
+    printf("%d    %d    %d",soma[0],soma[1],soma[2]);
+    
 }
